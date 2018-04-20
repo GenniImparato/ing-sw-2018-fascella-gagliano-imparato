6d923db998d3b61e464f6c5f3ecc927254bd37e9
GenniImparato <37338634+GenniImparato@users.noreply.github.com>
2018-04-20 15:45:34 +0200

    modifiche alla classe Die

--- src/main/java/it/polimi/se2018/game/Die.java
+++ src/main/java/it/polimi/se2018/game/Die.java
@@ -7,13 +7,18 @@ public class Die
     private Color       color;
     private int         value;
 
-    public Die()
+    public Die(Color color)
     {
-        Random random = new Random();
-        color = Color.getRandomColor();
-        value = random.nextInt(6)+1;
+        this.color = color;
+        roll();
     }
 
     public int getValue()   {return value;}
     public Color getColor() {return color;}
+
+    public void roll()
+    {
+        Random random = new Random();
+        value = random.nextInt(6)+1;
+    }
 }
