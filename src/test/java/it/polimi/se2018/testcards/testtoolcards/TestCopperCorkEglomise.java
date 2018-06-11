b5d00c7b82c978a5b619632fe8145839ef7742a1
carmelofascella <carmelo.fascella@mail.polimi.it>
2018-06-11 18:06:48 +0200

    Test CorkBacked ToolCard

--- src/test/java/it/polimi/se2018/testcards/testtoolcards/TestCopperCorkEglomise.java
+++ src/test/java/it/polimi/se2018/testcards/testtoolcards/TestCopperCorkEglomise.java
@@ -17,7 +17,10 @@ import static org.junit.Assert.*;
 
 import static org.junit.Assert.fail;
 
-public class TestToolCards
+/**
+ * Class used to test three Tool Cards: "Copper Foil Burnisher, Cork Backed Straightedge, Eglomise Brush"
+ */
+public class TestCopperCorkEglomise
 {
     private Model model;
     private Controller controller;
@@ -26,9 +29,15 @@ public class TestToolCards
     private SagradaSchemeCardFile sagradaSchemeCardFile;
     private int [] numCards;
 
+    /**
+     * Method that sets a game table with the three Tool Cards selected. ù
+     * It uses the Controller with the parameter of the path directory where there are the selected cards,
+     * and sets the index of the card in the array "numCards"
+     */
     @Before
     public void setUp()
     {
+
         model = new Model();
         controller = new Controller(model, "./test_resources/toolcards/firstdirectory/");
         view = new TestView();
@@ -73,6 +82,10 @@ public class TestToolCards
 
     }
 
+    /**
+     * Tests the use of the CopperFoilBurnisher, it tries to move a Die in a position ignoring the value restriction.
+     * Then it tries to add the same die in a position where there is a color restriction
+     */
     @Test
     public void useCopperFoilBurnisher()
     {
@@ -91,14 +104,46 @@ public class TestToolCards
 
         view.notify(new UseToolCardEvent(view, numCards[0]));           //use copper foil burnisher
 
-        view.notify(new SelectDieFromBoardEvent(view,0,0));
-
-        view.notify(new MoveSelectedDieEvent(view, 0,2));
+        view.notify(new SelectDieFromBoardEvent(view,0,0));     //select a Die just added on the board
+        view.notify(new MoveSelectedDieEvent(view, 0,2));       //and we add it in a position ignoring the value restriction
 
         assertEquals(die.getValue(), board.getDie(0,2).getValue());
 
 
+        view.notify(new SelectDieFromBoardEvent(view,0,2));     //select the Die
+        view.notify(new MoveSelectedDieEvent(view, 0,1));       //and we try to add it in a position with a different color restriction
+
+        assertEquals(null, board.getDie(0,1));
+
     }
+
+    @Test
+    public void useCorkBackedStraightedge()
+    {
+        Die die = new Die(Color.RED);
+        die.setValue(4);
+        try
+        {
+            board.addDie(die, 2,0,false,false,false);
+        }
+        catch(ChangeModelStateException|ActionNotPossibleException e)
+        {
+            fail();
+        }
+
+        view.notify(new UseToolCardEvent(view, numCards[1]));           //use cork backed straightedge
+        int value = model.getDraftPool().getAllDice().get(0).getValue();
+        view.notify(new DraftDieEvent(view, 0));
+
+
+        view.notify(new AddDieToBoardEvent(view, 2,4));     //add a die withouth respecting the adjacent restrictions
+
+        //assertNotEquals(null, board.getDie(2,1));
+        assertEquals(value, board.getDie(2,4).getValue());
+
+
+    }
+
 
 
 }
